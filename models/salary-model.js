26bf4ff0e33307842181a228883d267de326e4d0
rjmnhr1 <renjith.cm@refactor.academy>
2023-10-02 09:36:18 +0530

    update

--- models/salary-model.js
+++ models/salary-model.js
@@ -5,7 +5,7 @@ const SalaryModel = {
     const connection = await pool.getConnection();
 
     try {
-      const query = `SELECT * FROM naukri_extract LIMIT 100`;
+      const query = `SELECT mapped_job_title, salary, skill1, skill2, skill3, skill4 ,skill5, skill6, skill7, skill8, mapped_average_sal, "avg experience", comp_industry  FROM naukri_extract `;
 
       const [rows] = await connection.query(query);
 
