986cb8bfb9677510f74b883c7d20570b41b0f442
rjmnhr1 <renjith.cm@refactor.academy>
2023-10-06 21:09:50 +0530

    update

--- models/salary-model.js
+++ models/salary-model.js
@@ -5,11 +5,26 @@ const SalaryModel = {
     const connection = await pool.getConnection();
 
     try {
-      const query = `SELECT mapped_job_title, salary,  mapped_average_sal, avg_experience  FROM naukri_extract where mapped_job_title= '${getAll.job_title}' AND location LIKE '%${getAll.location}%' `;
+      // User input array of skills
+      const userInputSkills = getAll.skills;
+
+      const conditions = userInputSkills
+        .map((skill) => `FIND_IN_SET('${skill}', combined_skills) > 0`)
+        .join(" OR ");
+      const query = `SELECT mapped_job_title, mapped_job_title_1, current_date, salary,  mapped_average_sal, avg_experience  FROM naukri_extract WHERE (${conditions}) AND mapped_job_title= '${getAll.job_title}' AND location LIKE '%${getAll.location}%' `;
 
       const [rows] = await connection.query(query);
 
-      return rows;
+      const rowsCheck = rows;
+
+      if (rowsCheck.length > 0) {
+        return rows;
+      } else {
+        const query = `SELECT mapped_job_title,mapped_job_title_1, current_date, salary,  mapped_average_sal, avg_experience  FROM naukri_extract WHERE mapped_job_title= '${getAll.job_title}' AND location LIKE '%${getAll.location}%' `;
+        const [rows] = await connection.query(query);
+
+        return rows;
+      }
     } catch (err) {
       // Handle errors here
       console.error(err);
